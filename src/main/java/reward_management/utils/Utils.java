adcadbdbd01dea156bfba4848d9d33660610487e
Amodu <amodudavid777@gmail.com>
2024-09-08 19:03:29 +0100

    Feat: Added utils

---
+++ src/main/java/reward_management/utils/Utils.java
@@ -0,0 +1,10 @@
+package reward_management.utils;
+
+import java.util.UUID;
+
+public class Utils {
+
+    public static UUID stringToUuid(String string){
+        return UUID.fromString(string);
+    }
+}
