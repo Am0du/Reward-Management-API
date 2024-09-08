9938643a8bd7adba0d46d2b47d3844fba3a6eb79
Amodu <amodudavid777@gmail.com>
2024-09-08 19:36:36 +0100

    Fix: bugfix for unique constraint

--- src/main/resources/db/migration/V1__init_table.sql
+++ src/main/resources/db/migration/V1__init_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE users(
     id UUID UNIQUE NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    email UNIQUE VARCHAR(255) NOT NULL,
+    email  VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
